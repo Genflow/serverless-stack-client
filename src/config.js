535844a5f32515772fa81047b202b36ba5f3d98c
roland.bicaku <roland_bicaku@yahoo.com>
2020-04-19 19:07:47 +0100

    Setting up our React app

---
+++ src/config.js
@@ -0,0 +1,16 @@
+export default {
+    s3: {
+        REGION: "eu-west-1",
+        BUCKET: "notes-app-uploads-test-test"
+    },
+    apiGateway: {
+        REGION: "eu-west-1",
+        URL: "https://kj99qmlvp9.execute-api.eu-west-1.amazonaws.com/prod"
+    },
+    cognito: {
+        REGION: "eu-west-1",
+        USER_POOL_ID: "eu-west-1_QYR0jTjlY",
+        APP_CLIENT_ID: "6q1r3uu25e0qj8fdqbjoj32257",
+        IDENTITY_POOL_ID: "eu-west-1:f5b2c298-0a3d-475b-9704-c10a356c5886"
+    }
+};
